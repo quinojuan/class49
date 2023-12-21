e358edef1355a3b99f96f1c14016f2aa3a1ef587
quinojuan <quinojuan@gmail.com>
2023-12-21 09:34:41 -0300

    Flex direction

--- src/screens/FlexScreen.tsx
+++ src/screens/FlexScreen.tsx
@@ -11,27 +11,33 @@ export const FlexScreen = () => {
   );
 };
 
+// Si tengo mas de un elemento el flex es compartido por los hijos.
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: 'violet',
+    flexDirection: 'row'
   },
   caja1: {
-    flex: 5,
+    // flex: 5, // 5 + 3 + 2 = 10 (así que 5 es la mitad del total o sea, el 50%)
+    flex: 1,
     borderWidth: 2,
     borderColor: "white",
     backgroundColor: "#f0a23b",
     fontSize: 30
   },
   caja2: {
-    flex: 3,
+    // flex: 3,
+    flex: 1,
     borderWidth: 2,
     backgroundColor: "#5856d6",
     borderColor: "white",
     fontSize: 30
   },
   caja3: {
-    flex: 2,
+    // flex: 2,
+    flex: 1,
     borderWidth: 2,
     backgroundColor: "#28c4d9",
     borderColor: "white",
