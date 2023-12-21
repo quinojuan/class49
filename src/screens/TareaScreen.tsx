56a0fa24b9a812786b3d4fd8b9b525b645408412
quinojuan <quinojuan@gmail.com>
2023-12-21 14:47:30 -0300

    alternativa al ejercicio 5

--- src/screens/TareaScreen.tsx
+++ src/screens/TareaScreen.tsx
@@ -15,29 +15,24 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#284258',
-justifyContent: 'center',
-alignItems: 'center',
+justifyContent: 'space-between',
 flexDirection: 'row'
     },
   cajaMorada: {
 width: 100,
-height: 100,
 borderWidth: 10,
 borderColor: 'white',
 backgroundColor: '#5856D6',
 },
 cajaNaranja: {
     width: 100,
-    height: 100,
     borderWidth: 10,
     borderColor: 'white',
     backgroundColor: '#f0a23b',
-    top: 50
     
   },
   cajaAzul: {
     width: 100,
-    height: 100,
     borderWidth: 10,
     borderColor: 'white',
     backgroundColor: 'blue',
