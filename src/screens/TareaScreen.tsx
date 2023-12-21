c48b3226dcb5597decf22a946bfeda5d14aa1010
quinojuan <quinojuan@gmail.com>
2023-12-21 14:12:33 -0300

    Clase 81 ejercicio2

--- src/screens/TareaScreen.tsx
+++ src/screens/TareaScreen.tsx
@@ -15,6 +15,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#284258',
+    justifyContent: 'center'
   },
   cajaMorada: {
     width: 100,
@@ -26,16 +27,15 @@ const styles = StyleSheet.create({
   cajaNaranja: {
     width: 100,
     height: 100,
-    flex: 1,
     borderWidth: 10,
     borderColor: 'white',
     backgroundColor: '#f0a23b',
   },
   cajaAzul: {
-    width: 100,
+    width: "100%",
     height: 100,
     borderWidth: 10,
     borderColor: 'white',
-    backgroundColor: 'blue',
+    backgroundColor: 'blue'
   },
 });
